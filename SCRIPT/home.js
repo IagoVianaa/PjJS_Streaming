ac368523383dd7422b748d853728b8930cbab5b3
Gabriel Lins Soares Silvestre <gabriel.lins.silvestre@gmail.com>
2023-06-07 17:19:58 -0300

    Melhorando css do site / carrousel bootstrap

--- SCRIPT/home.js
+++ SCRIPT/home.js
@@ -1,80 +1,16 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 5000
-  },
-});
+let items = document.querySelectorAll('.carousel .carousel-item')
 
-const apiKey = 'd464a5d67c9f591617622782ed3f4d87';
-
-// URL base da API do TMDb
-const baseUrl = 'https://api.themoviedb.org/3';
-
-// Puxa filmes da API do TMDb
-function fetchMovies() {
-  const url = `${baseUrl}/movie/popular?api_key=${apiKey}`;
-
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => data.results.slice(0, 4))
-    .catch(error => {
-      console.error('Erro ao buscar filmes:', error);
-      return [];
-    });
-}
-
-// Cria os slides do Swiper com os filmes
-function createSwiperSlides(movies) {
-  const swiperWrapper = document.getElementById('swiper-wrapper');
-
-  movies.forEach(movie => {
-    const slide = document.createElement('div');
-    slide.classList.add('swiper-slide');
-
-    const movieTitle = document.createElement('div');
-    movieTitle.classList.add('movie-title');
-    movieTitle.textContent = movie.title;
-
-    const moviePoster = document.createElement('img');
-    moviePoster.classList.add('movie-poster');
-    moviePoster.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
-    moviePoster.alt = movie.title;
-
-    slide.appendChild(moviePoster);
-    slide.appendChild(movieTitle);
-    swiperWrapper.appendChild(slide);
-  });
-}
-
-// Inicializa o Swiper
-function initializeSwiper() {
-  new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
     }
-  });
-}
-
-// Função principal
-async function main() {
-  const movies = await fetchMovies();
-  createSwiperSlides(movies);
-  initializeSwiper();
-}
-
-main();
-
+})
 
